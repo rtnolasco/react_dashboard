94390cc42caccf81c75117b7fe677142c83ed16d
Developer <developer@Developers-MacBook-Pro.local>
2023-04-19 12:13:14 +0800

    create context provider state management

--- src/App.js
+++ src/App.js
@@ -32,10 +32,13 @@ import {
 	Line,
 } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 function App() {
-	const activeMenu = true;
+	const { activeMenu } = useStateContext();
+
 	return (
 		<div>
 			<BrowserRouter>
